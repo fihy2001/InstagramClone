1650b236803634f432a0903be8a0a15456546e0b
James <fihy2001@hotmail.com>
2020-06-16 11:36:57 -0400

    Completed All Picture upload and menu items

--- app/src/main/java/com/example/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/instagramclone/UsersTab.java
@@ -1,9 +1,11 @@
 package com.example.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -18,14 +21,17 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UsersTab extends Fragment {
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView listView;
-    private ArrayList arrayList;
+    private ArrayList<String> arrayList;
     private ArrayAdapter arrayAdapter;
 
     public UsersTab() {
@@ -42,6 +48,9 @@ public class UsersTab extends Fragment {
         listView = view.findViewById(R.id.listView);
         arrayList = new ArrayList();
         arrayAdapter = new ArrayAdapter (getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setOnItemClickListener(UsersTab.this);
+        listView.setOnItemLongClickListener(UsersTab.this);
 
         final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
 
@@ -70,5 +79,50 @@ public class UsersTab extends Fragment {
 
 
         return view;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Intent intent = new Intent(getContext(), UsersPosts.class);
+        intent.putExtra("username", arrayList.get(position));
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null && e == null){
+ //                   FancyToast.makeText(getContext(), user.get("profileProfession") + "", Toast.LENGTH_SHORT,
+  //                          FancyToast.SUCCESS, false).show();
+
+                    final PrettyDialog prettyDialog = new PrettyDialog(getContext());
+
+                    prettyDialog.setTitle(user.getUsername() + "'s Info")
+                            .setMessage(user.get("profileBio") + "\n" +
+                                        user.get("profileProfession") + "\n" +
+                                        user.get("profileHobbies") + "\n" +
+                                        user.get("profileFavoriteSport"))
+                            .setIcon(R.drawable.person)
+                            .addButton("OK", R.color.pdlg_color_white, R.color.pdlg_color_green, new PrettyDialogCallback() {
+                                @Override
+                                public void onClick() {
+
+                                    prettyDialog.dismiss();
+                                }
+                            })
+                            .show();
+
+                }
+            }
+        });
+
+
+        return true;
     }
 }
